e6f9b890bbc1a9582d28f56af4ad53d8fc0e48b0
Bart Vries <BVR@afasgroep.nl>
2025-10-22 15:50:23 +0200

    Add orleans host

---
+++ OrleansHost/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.UseOrleans(siloBuilder => siloBuilder.UseLocalhostClustering());
+
+using var host = builder.Build();
+host.Start();
+
+Console.WriteLine("Orleans host started. Press Enter to terminate...");
+Console.ReadLine();
